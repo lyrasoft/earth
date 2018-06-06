2d36ff2351e6f30435d7e0f50ce785d95644ea60
Simon Asika <asika32764@gmail.com>
2018-06-06 16:14:08 +0800

    Update for repo

--- src/Admin/Controller/Dashboard/GetController.php
+++ src/Admin/Controller/Dashboard/GetController.php
@@ -11,8 +11,9 @@ namespace Admin\Controller\Dashboard;
 use Admin\Model\DashboardModel;
 use Admin\View\Dashboard\DashboardHtmlView;
 use Phoenix\Controller\Display\DisplayController;
-use Windwalker\Core\Model\ModelRepository;
+use Windwalker\Core\Repository\Repository;
 use Windwalker\Core\View\AbstractView;
+use Windwalker\Debugger\Repository\DashboardRepository;
 
 /**
  * The GetController class.
@@ -46,6 +47,7 @@ class GetController extends DisplayController
      * A hook before main process executing.
      *
      * @return  void
+     * @throws \Exception
      */
     protected function prepareExecute()
     {
@@ -53,23 +55,25 @@ class GetController extends DisplayController
     }
 
     /**
-     * Prepare view and default model.
+     * Prepare view and default repository.
      *
      * You can configure default model state here, or add more sub models to view.
      * Remember to call parent to make sure default model already set in view.
      *
-     * @param AbstractView    $view  The view to render page.
-     * @param ModelRepository $model The default mode.
+     * @param AbstractView $view       The view to render page.
+     * @param Repository   $repository The default repository.
      *
      * @return  void
+     *
+     * @since  __DEPLOY_VERSION__
      */
-    protected function prepareViewModel(AbstractView $view, ModelRepository $model)
+    protected function prepareViewRepository(AbstractView $view, Repository $repository)
     {
         /**
-         * @var $view  DashboardHtmlView
-         * @var $model DashboardModel
+         * @var $view       DashboardHtmlView
+         * @var $repository DashboardRepository
          */
-        parent::prepareViewModel($view, $model);
+        parent::prepareViewRepository($view, $repository);
 
         // Configure View nad Models here
     }
