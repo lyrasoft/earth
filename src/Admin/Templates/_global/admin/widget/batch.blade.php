8e3aef186c954a16cd3cb0957e5b7f7236939f88
Simon Asika <asika32764@gmail.com>
2019-06-29 22:54:20 +0800

    New batch

--- src/Admin/Templates/_global/admin/widget/batch.blade.php
+++ src/Admin/Templates/_global/admin/widget/batch.blade.php
@@ -1,4 +1,4 @@
-{{-- Part of Admin project. --}}
+{{-- Part of earth project. --}}
 <?php
 /**
  * Global variables
@@ -7,49 +7,60 @@
  * @var $package  \Windwalker\Core\Package\AbstractPackage    Package object.
  * @var $view     \Windwalker\Data\Data                       Some information of this view.
  * @var $uri      \Windwalker\Uri\UriData                     Uri information, example: $uri->path
- * @var $datetime \DateTime                                   PHP DateTime object of current time.
+ * @var $chronos  \Windwalker\Core\DateTime\Chronos           PHP DateTime object of current time.
  * @var $helper   \Windwalker\Core\View\Helper\Set\HelperSet  The Windwalker HelperSet object.
- * @var $route    \Windwalker\Core\Router\PackageRouter       Route builder object.
+ * @var $router   \Windwalker\Core\Router\PackageRouter       Route builder object.
  * @var $asset    \Windwalker\Core\Asset\AssetManager         The Asset manager.
  *
- * @var $form      \Windwalker\Form\Form
+ * ---------------------------------------------------------------
+ * @var $form     \Windwalker\Form\Form
  */
-?>
 
+$update = isset($update) ? $update : true;
+$copy = isset($copy) ? $copy : true;
+?>
+<style>
+    body.bootstrap-3 #batch-modal .close {
+        position: relative;
+        top: -25px;
+    }
+</style>
 <div class="modal fade" id="batch-modal" tabindex="-1" role="dialog" aria-labelledby="batch-modal-title">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title" id="batch-modal-label">
-                    <span class="glyphicon glyphicon-modal-window fa fa-sliders"></span>
-                    @translate('phoenix.batch.modal.title')
+                <h4 class="modal-title" id="batch-modal-title">
+                    <span class="fa fa-sliders"></span> @lang('phoenix.batch.modal.title')
                 </h4>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+                    <span aria-hidden="true">&times;</span></button>
             </div>
             <div class="modal-body">
                 <p>
-                    @translate('phoenix.batch.modal.desc')
+                    @lang('phoenix.batch.modal.desc')
                 </p>
-                <hr/>
+                <hr />
                 <div class="form-horizontal">
                     {!! $form->renderFields(null, 'batch') !!}
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">
-                    <span class="glyphicon glyphicon-remove fa fa-remove"></span>
-                    @translate('phoenix.core.close')
+                <button type="button" role="button" class="btn btn-default btn-outline-secondary" data-dismiss="modal">
+                    <span class="fa fa-remove fa-times"></span>
+                    @lang('phoenix.core.close')
                 </button>
-                <button type="button" class="btn btn-info" onclick="Phoenix.Grid.hasChecked();Phoenix.patch()">
-                    <span class="glyphicon glyphicon-ok fa fa-check"></span>
-                    @translate('phoenix.core.update')
-                </button>
-                <button type="button" class="btn btn-primary" onclick="Phoenix.Grid.hasChecked();Phoenix.post()">
-                    <span class="glyphicon glyphicon-duplicate fa fa-copy"></span>
-                    @translate('phoenix.core.copy')
-                </button>
+                @if ($update)
+                    <button type="button" class="btn btn-info" onclick="Phoenix.Grid.hasChecked();Phoenix.patch()">
+                        <span class="fa fa-check"></span>
+                        @lang('phoenix.core.update')
+                    </button>
+                @endif
+                @if ($copy)
+                    <button type="button" class="btn btn-primary" onclick="Phoenix.Grid.hasChecked();Phoenix.post()">
+                        <span class="fa fa-copy"></span>
+                        @lang('phoenix.core.copy')
+                    </button>
+                @endif
             </div>
         </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
