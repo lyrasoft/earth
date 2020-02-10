6d51119f220b87907177cdc589e4aa4f83c4d3c0
Simon Asika <asika32764@gmail.com>
2020-02-10 13:35:04 +0800

    Add UA IE=11 #49

--- src/Front/Templates/_global/html.blade.php
+++ src/Front/Templates/_global/html.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
     <title>{{ \Phoenix\Html\HtmlHeader::getPageTitle() }}</title>
 
