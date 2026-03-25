2be7b0feaab3e340f318adf4c1b5be380d5d3cb1
Simon Asika <asika32764@gmail.com>
2026-03-25 19:41:59 +0800

    ignore env

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import { defineConfig } from 'vite';
 export default defineConfig(({ mode }) => {
   return {
     base: './',
+    server: {
+      watch: {
+        ignored: ['**/.env*']
+      }
+    },
     build: {
       sourcemap: false,
       minify: mode === 'production',
